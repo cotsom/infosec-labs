2e6f9413030886e849d0c26b86bc3af55c4875f8
cotsom <gerfy.17@mail.ru>
2023-01-06 22:40:06 +0300

    update web

--- websocket/app.py
+++ websocket/app.py
@@ -1,18 +1,36 @@
+from functools import wraps
 import os
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, redirect, render_template, request, session, url_for
 from flask_sock import Sock
-import mysql.connector
+import mariadb
 
 
 app = Flask(__name__)
+app.secret_key = 'g8y348f3h4f34jf93ij4g3u49gh3487fh34fj8347hfg3487fh348jf34hf837fg'
 sock = Sock(app)
 config = {
-    'user': 'root',
-    'password': 'password',
-    'host': 'mariadb',
-    'database': 'users',
-    'raise_on_warnings': True
+    "user": "root",
+    "password": "password",
+    "host": "mariadb",
+    "port": 3306,
+    "database": "users"
     }
+
+def dbConn():
+    try:
+        conn = mariadb.connect(**config)
+    except mariadb.Error as e:
+        print(f"Error connecting to MariaDB Platform: {e}")
+    cur = conn.cursor()
+    return cur
+
+def auth(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'user' not in session:
+            session['user'] = False
+        return f(*args, **kwargs)
+    return decorated_function
     
 @app.route("/")
 def index():
@@ -26,20 +44,54 @@ def echo(sock):
         domen = os.popen("nslookup " + data).read()
         sock.send(domen)
 
-@app.route("/admin-page", methods=["GET", "POST"])
+@app.route("/admin-login", methods=["GET", "POST"])
 def adminLogin():
-    print("TESTTESTTESTTESTTESTTESTTESTTSETTSETTEST")
     if request.method == 'POST':
-        cnx = mysql.connector.connect(**config)
-        cursor = cnx.cursor()
-
+        username = ''
+        cur = dbConn()
         login = request.form['login']
         password = request.form['password']
-        query = (f"SELECT * FROM users_info WHERE login = '{login}' AND password = '{password}'")
-        user = cursor.execute(query)
-        print(user)
-        cnx.close()
+
+        cur.execute(
+            f"SELECT * FROM users_info WHERE login = '{login}' AND password = '{password}'"
+            )
+
+        for (id, log, pas, voc) in cur:
+            username = log
+            print(id, log, pas, voc)
+        
+        if username:
+            session['user'] = username
+            return redirect(url_for('adminPage'))
+        else: 
+            return render_template('admin-login.html', error="invalid credentials")
+
     return render_template('admin-login.html')
+
+@auth
+@app.route("/admin-page", methods=["GET", "POST"])
+def adminPage():
+    is_user = session.get('user', False)
+    if not (is_user):
+        return redirect(url_for('adminLogin'))
+
+    cur = dbConn()
+    employee = request.args.get('employee')
+    vocation = ''
+
+    cur.execute(
+        f"SELECT vocation FROM users_info WHERE login = '{employee}'"
+        )
+    for voc in cur:
+        vocation = voc
+        print(voc)
+    
+    if vocation:
+        employee_name = employee
+    else:
+        print("user not found")
+
+    return render_template("admin-page.html", employee=employee_name, vocation=vocation)
 
 
 if __name__ == "__main__":
--- websocket/sql/database.sql
+++ websocket/sql/database.sql
@@ -16,14 +16,17 @@ SET time_zone = "+00:00";
 CREATE TABLE `users_info` (
   `id` int UNSIGNED NOT NULL,
   `login` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `password` varchar(32) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL
+  `password` varchar(32) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `vocation` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3;
 
 
-INSERT INTO `users_info` (`id`, `login`, `password`) VALUES
-(1, 'admin', 'hguerhg@!r32hf2buJHfe');
-INSERT INTO `users_info` (`id`, `login`, `password`) VALUES
-(2, 'moderator', 'oijrgoih34g234');
+INSERT INTO `users_info` (`id`, `login`, `password`, `vocation`) VALUES
+(1, 'admin', 'hguerhg@!r32hf2buJHfe', 'Devops');
+INSERT INTO `users_info` (`id`, `login`, `password`, `vocation`) VALUES
+(2, 'moderator', 'oijrgoih34g234', 'Backend programmer');
+INSERT INTO `users_info` (`id`, `login`, `password`, `vocation`) VALUES
+(3, 'Sam', 'oijh348ufh34f234', 'Frontend programmer');
 
 --
 -- Индексы таблицы `users_info`
