b9bc29afbaecf0c3aa2062b3f35752c71dc79a26
cotsom <gerfy.17@mail.ru>
2023-01-31 22:26:36 +0300

    upd

---
+++ privesc-vim/app.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask, render_template
+from flask_sock import Sock
+
+app = Flask(__name__)
+sock = Sock(app)
+    
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+@sock.route("/echo")
+def echo(sock):
+    while True:
+        data = sock.receive()
+        domen = os.popen("nslookup " + data).read()
+        sock.send(domen)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=False)
